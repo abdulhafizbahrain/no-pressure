95bb3106510ee2fd8b85b951c093334712a3d89a
Abdul Hafiz Bahrain <abdulhafizbahrain@gmail.com>
2023-05-09 17:27:29 +0800

    fix pins button

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,14 @@
 <template>
     <section class="min-h-screen bg-stone-300 relative">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.user" :href="route('pins')" class="text-sm text-gray-700 dark:text-gray-500 underline">Pins</Link>
+        <div v-if="canLogin" class="fixed top-0 right-0 px-6 py-4 sm:block">
+            <Link v-if="$page.props.user" :href="route('pins')" class="text-sm text-gray-700 dark:text-gray-500 underline">
+            Pins</Link>
 
             <template v-else>
                 <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
+                <Link v-if="canRegister" :href="route('register')"
+                    class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
             </template>
         </div>
 
@@ -32,7 +34,7 @@ defineProps({
     phpVersion: String,
     pins: {
         Type: Object,
-        default: () => {}
+        default: () => { }
     }
 });
 </script>
